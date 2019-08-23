024cd7d98c9792572b3c3c1b61ff9e61e217dafb
Junaid Khan <junaidkhan1@gmx.com>
2019-08-23 15:39:35 +0100

    add slider without style

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import ColorBox from './ColorBox'
 class Palette extends Component {
-  state = { }
+  state = { 
+    level: 500
+  }
+
+  changeLevel = (newLevel)=>{
+    this.setState({
+      level: newLevel
+    })
+  }
   render () {
-    const colorsBoxes = this.props.palette.colors[300].map(box => {
+    const {level} = this.state
+    const colorsBoxes = this.props.palette.colors[level].map(box => {
       return (
         <ColorBox key={box.name} background={box.hex} name={box.name} />
       )
     })
     return (
       <div className='Palette'>
+        <Slider defaultValue={level} min={100} max={900}
+         onAfterChange={this.changeLevel} step={100} />
         <div className='Palette-colors'>
           {colorsBoxes}
-
         </div>
         {/* footer here */}
       </div>
