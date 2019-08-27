a9f0f58308ea5267c92a8a65ded71488acca5b5f
Junaid Khan <junaidkhan1@gmx.com>
2019-08-27 12:29:51 +0100

    styled select and snackbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider'
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
 class Navbar extends Component {
   state = { 
     localLevel : 500,
-    format: 'hex'
+    format: 'hex',
+    open: false
    }
   change = (nwLevel) => {
 
@@ -21,11 +25,17 @@ class Navbar extends Component {
 
   changeFormat = (e) => {
     this.setState({
-      format: e.target.value
+      format: e.target.value,
+      open: true
     }, ()=>{this.props.handleChange(e)})
     
   }
 
+  closeSnackbar = (e) =>{
+this.setState({
+  open: false
+})
+  }
   render() { 
 
 
@@ -46,6 +56,12 @@ class Navbar extends Component {
             <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar anchorOrigin={ {vertical: 'bottom', horizontal : 'left'} } 
+        open={this.state.open} autoHideDuration={3000} message={<span id='message-id'>Format Changed to {this.state.format} !</span>} action={[
+          <IconButton onClick={this.closeSnackbar} color='inherit' key='close'>
+            <CloseIcon />
+          </IconButton>
+        ]} onClose={this.closeSnackbar}/>
       </nav>
      )
   }
