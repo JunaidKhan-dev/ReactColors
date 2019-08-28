ed222a8a05e8a06f34970d67bcd2c0d6b8c01447
Junaid Khan <junaidkhan1@gmx.com>
2019-08-28 13:29:56 +0100

    Updates

---
+++ precache-manifest.1a8d2c2ed05c8aac745c7e1b21da23d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96da066d39b8067719bbf712d5c0689f",
+    "url": "/ReactColors/index.html"
+  },
+  {
+    "revision": "813c4a59c7ea9e3da4ff",
+    "url": "/ReactColors/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "66cc9f212351df961e40",
+    "url": "/ReactColors/static/css/main.e7eaf73d.chunk.css"
+  },
+  {
+    "revision": "813c4a59c7ea9e3da4ff",
+    "url": "/ReactColors/static/js/2.22bd6bd3.chunk.js"
+  },
+  {
+    "revision": "66cc9f212351df961e40",
+    "url": "/ReactColors/static/js/main.36ee3b83.chunk.js"
+  },
+  {
+    "revision": "dbc67f7dcf4a72ddc8cc",
+    "url": "/ReactColors/static/js/runtime~main.c2db4e01.js"
+  }
+]);
